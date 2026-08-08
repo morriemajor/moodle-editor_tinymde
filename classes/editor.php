c4737798efa34dd2928cf64572d8a200fd05d47c
morriemajor <andrew.bainbridge-smith@canterbury.ac.nz>
2026-08-08 15:20:18 +1200

    Fix remaining Code Checker blank-line-after-brace warnings in classes/editor.php and classes/privacy/provider.php

--- classes/editor.php
+++ classes/editor.php
@@ -29,7 +29,6 @@ namespace editor_tinymde;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class editor extends \texteditor {
-
     #[\Override]
     public function supported_by_browser() {
         return true;
